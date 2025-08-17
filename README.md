de57acb4a9713bb5838e5c72a6f3f7f6858b8f69
Your Name <you@example.com>
2025-08-17 23:02:46 +0800

    add uv as dep manager

--- README.md
+++ README.md
@@ -4,6 +4,14 @@
 
 python3 -m venv venv && source ./venv/bin/activate
 
+## install dependency
+
+curl -LsSf https://astral.sh/uv/install.sh | sh
+
+### install according to pyproject.toml
+
+uv sync
+
 ## examples
 
 https://docs.tplr.ai/
---
+++ basic_ml/gradient.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def f(x):  # 目标函数
+    return x ** 2
+
+def f_grad(x):  # 目标函数的梯度(导数)
+    return 2 * x
+
+def gd(eta, f_grad):
+    x = 10.0
+    results = [x]
+    for i in range(10):
+        x -= eta * f_grad(x)
+        results.append(float(x))
+    print(f'epoch 10, x: {x:f}')
+    return results
+
+results = gd(0.2, f_grad)
+print(results)
+
